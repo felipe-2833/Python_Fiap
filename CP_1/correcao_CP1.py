9a487f2e8520b5ffcbf4a68c844996b4788bb5ea
alicia.guiradelo <alicia.guiradelo@outlook.com>
2024-04-09 10:21:44 -0300

    correção prof

---
+++ CP_1/correcao_CP1.py
@@ -0,0 +1,4 @@
+#replace - troca um valor por outro -> login.replace("r", "R") -> troca "r" por "R"
+#enumerate - for i, c in enumerate("abc") -> vai enumerar a quantidade de caractres
+# click + F2 -> renomeia todoscom o mesmo nome
+# "|" em match case é "or"
